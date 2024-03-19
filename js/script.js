2640cc65df0010f9fffbe7b43381a43f9c278526
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-03-19 16:35:46 +0100

    add: function for generate random number

--- js/script.js
+++ js/script.js
@@ -1,1 +1,50 @@
-alert('test')
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+// Consigli del giorno:
+// * Pensate prima in italiano.
+// * Dividete in piccoli problemi la consegna.
+// * Individuate gli elementi di cui avete bisogno per realizzare il programma.
+
+// array vuoto riempito da numeri casuali n 5
+// poi avviare timer da 30 secondi
+// dopo i 30 secondi far scomparire l'array ,
+// poi con un prompt inserire i 5 numeri 
+
+
+const myNumber = document.getElementById('#number-random')
+
+function generalFunc(){
+    // array vuoto riempito con 5 numeri 
+    const numberSoftwar = generateRandomArray(5,0,100);
+    console.log(numberSoftwar);
+};
+
+
+
+
+
+
+
+
+function generateRandomArray(arrayLength, numMin, numMax) {
+    // Creiamo un array vuoto
+    const randomNumbersArray = [];
+
+    // finche non ci sono arrayLength numeri nell'array:
+    while(randomNumbersArray.length < arrayLength) {
+        // genero un numero random
+        const randNumber = getRndInteger(numMin, numMax);
+        // se il numero random non esiste nell'array lo pusho
+        if(!randomNumbersArray.includes(randNumber)) {
+            randomNumbersArray.push(randNumber);
+        }
+    }
+    
+    return randomNumbersArray;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
